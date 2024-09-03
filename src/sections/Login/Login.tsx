0b5128866ba2ca923d286c75d3876ced06d91104
Dave <davekrelak@hotmail.com>
2024-09-03 10:34:15 +0200

    Login with css

--- src/sections/Login/Login.tsx
+++ src/sections/Login/Login.tsx
@@ -1,12 +1,13 @@
 import React, {useState} from "react";
 import {UserRepository} from "../../domain/User/UserRepository";
+import styles from "./Login.module.scss";
 
-export function Login({userRepository, setToken}: {userRepository: UserRepository, setToken:any}) {
+export function Login({userRepository, setToken}: { userRepository: UserRepository, setToken: any }) {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
 
-    const handleSubmit = async (e:React.FormEvent<HTMLFormElement>) => {
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         const token = await userRepository.login({
             email,
@@ -15,19 +16,27 @@ export function Login({userRepository, setToken}: {userRepository: UserRepositor
         setToken(token);
     }
 
-    return <div className="login-wrapper">
-        <h1>Hay que logarse gipi!</h1>
+    return <div className={styles.login}>
+        <h1>Esto es zona privada!</h1>
         <form onSubmit={handleSubmit}>
-            <label>
-                <p>Email</p>
-                <input type="text" onChange={e => setEmail(e.target.value)}/>
-            </label>
-            <label>
-                <p>Password</p>
-                <input type="password" onChange={e => setPassword(e.target.value)}/>
-            </label>
+            <div className={styles.text_area}>
+                <input
+                    type="text"
+                    name="username"
+                    className={styles.text_input}
+                    placeholder="email"
+                    onChange={e => setEmail(e.target.value)}/>
+            </div>
+            <div className={styles.text_area}>
+                <input
+                    type="password"
+                    name="password"
+                    className={styles.text_input}
+                    placeholder="password"
+                    onChange={e => setPassword(e.target.value)}/>
+            </div>
             <div>
-                <button type="submit">Submit</button>
+                <input type="submit" value="Login" className={styles.btn} />
             </div>
         </form>
     </div>
